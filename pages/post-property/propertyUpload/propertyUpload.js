6ee06ea3a9473d0820540350580059241d47b4ef
Chandan1999 <chandan@firstfloor.agency>
2022-06-20 16:11:11 +0530

    propertyupload pages changes

--- pages/post-property/propertyUpload/propertyUpload.js
+++ pages/post-property/propertyUpload/propertyUpload.js
@@ -5,6 +5,8 @@ import Image from "next/image";
 import * as Yup from "yup";
 import { useFormik } from "formik";
 import upload from "../../../assets/icons/upload.png";
+import backButton from "../../../assets/icons/backbutton.png";
+
 
 const PropertyUpload = () => {
   const [value, setValue1] = useState("4");
@@ -16,36 +18,38 @@ const PropertyUpload = () => {
     initialValues: {
       image: "",
       description: "",
-      terms: "",
+      terms: false,
     },
     validationSchema: Yup.object({
       image: Yup.mixed().required("Upload an Image"),
       description: Yup.string().required("Required"),
+      terms: Yup.boolean().oneOf(
+        [true],
+        "Terms and condition need to be accepted"
+      ),
 
-      terms: Yup.string().required("Required"),
+      // terms: Yup.string().required("Required"),
     }),
 
     onSubmit: (values, { resetForm }) => {
       console.log(values);
       resetForm();
-
-      onHide();
     },
   });
 
-  function handleChange(e) {
-    if (e.target?.files.length !== 0) {
-      setSize(100);
-      setuploaded(true);
-      setFile(URL.createObjectURL(e.target.files[0]));
-    }
-  }
+  // function handleChange(e) {
+  //   if (e.target?.files.length !== 0) {
+  //     setSize(100);
+  //     setuploaded(true);
+  //     setFile(URL.createObjectURL(e.target.files[0]));
+  //   }
+  // }
 
-  function formReset() {
-    setuploaded(false);
-    setSize(35);
-    formik.setFieldValue("image", "");
-  }
+  // function formReset() {
+  //   setuploaded(false);
+  //   setSize(35);
+  //   formik.setFieldValue("image", "");
+  // }
 
   return (
     <div>
@@ -99,25 +103,32 @@ const PropertyUpload = () => {
                     Photos
                   </label>
                   <div className={`${styles.upload_image} mt-3`}>
-                    {/* <input
-              type={"file"}
-              style={{ visibility: "hidden" }}
-              id="profile"
-              name="img"
-              className={`${styles.address} form-control`}
-              accept="image/*;capture=camera"
-           
-              onChange={(e) => {
-                formik.setFieldValue("image", e.target.files[0]);
-                handleChange(e);
-              }}
-            /> */}
-                    <div className="d-flex justify-content-center mt-3 mb-2">
-                      <Image src={upload} name="image"
-                      value={formik.values.image}
-                      alt="image of camera" />
+                    <div
+                      htmlFor="upload"
+                      className="d-flex justify-content-center mt-3 mb-2"
+                    >
+                      <label htmlFor="upload">
+                        {" "}
+                        <Image
+                          src={upload}
+                          name="image"
+                          value={formik.values.image}
+                          alt="image of camera"
+                        />
+                      </label>
                     </div>
                     <div className="d-flex flex-column align-items-center justify-content-center ">
+                      <input
+                        type={"file"}
+                        // style={{ visibility: "hidden" }}
+                        id="upload"
+                        name="image"
+                        accept="image/*;capture=camera"
+                       
+                        onChange={(e) => {
+                          formik.setFieldValue("image", e.target.files[0]);
+                        }}
+                      />
                       <p className={`${styles.upload_text_1}`}>
                         Drag & drop the image of an property
                       </p>
@@ -129,14 +140,15 @@ const PropertyUpload = () => {
                         <span className={`${styles.or_text}`}>-</span>OR
                         <span>-</span>
                       </p>
+                      
                       <p className={`${styles.upload_text_3}`}>Browse Photos</p>
                     </div>
                     {formik.errors.image && formik.touched.image && (
-              <div className="d-flex align-items-center text-danger mt-1">
-                <i className="ri-error-warning-line me-1 0 "></i>
-                <span>{formik.errors.image}</span>
-              </div>
-            )}
+                      <div className="d-flex align-items-center text-danger mt-1">
+                        <i className="ri-error-warning-line me-1 0 "></i>
+                        <span>{formik.errors.image}</span>
+                      </div>
+                    )}
 
                     {/* <input
                     type={text}
@@ -147,78 +159,89 @@ const PropertyUpload = () => {
                   /> */}
                   </div>
                 </div>
-                <div className="mt-3">
-                  <label
-                    for="exampleFormControlInput1"
-                    className={`${styles.labelText} form-label`}
-                  >
-                    Description
-                  </label>
-                  <textarea
-                    type="text"
-                
-                    className={`${styles.description} form-control`}
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Describe your property"
-                    name="description"
-                    value={formik.values.description}
-                    onChange={formik.handleChange}
-                  ></textarea>
+                <div className="mt-5">
+                  <div className="mt-3">
+                    <label
+                      for="exampleFormControlInput1"
+                      className={`${styles.labelText} form-label`}
+                    >
+                      Description
+                    </label>
+                    <textarea
+                      type="text"
+                      className={`${styles.description} form-control`}
+                      id="exampleFormControlInput1"
+                      aria-describedby="emailHelp"
+                      placeholder="Describe your property"
+                      name="description"
+                      value={formik.values.description}
+                      onChange={formik.handleChange}
+                    ></textarea>
+                  </div>
                 </div>
                 {formik.errors.description && formik.touched.description && (
-              <div className="d-flex align-items-center text-danger mt-1">
-                <i className="ri-error-warning-line me-1 0 "></i>
-                <span>{formik.errors.description}</span>
-              </div>
-            )}
+                  <div className="d-flex align-items-center text-danger mt-1">
+                    <i className="ri-error-warning-line me-1 0 "></i>
+                    <span>{formik.errors.description}</span>
+                  </div>
+                )}
                 <div className={` mt-2 `}>
                   <input
                     type="checkbox"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
+                    // name="terms"
+                    name="terms"
+                    onChange={formik.handleChange}
                   />
                   <span className={`ms-2 ${styles.terms_condition_text}`}>
                     Terms and condition
                   </span>
                 </div>
-              </form>
+                {formik.errors.terms && formik.touched.terms && (
+                  <div className="d-flex align-items-center text-danger mt-1">
+                    <i className="ri-error-warning-line me-1 0 "></i>
+                    <span>{formik.errors.terms}</span>
+                  </div>
+                )}
 
-              <div className={`content-btn d-flex justify-content-end mt-5`}>
-                <div className={`d-flex`}>
-                  <button className={`bg-white border-0  `}>
-                    <span
-                      className={`${styles.container_icon_arrowLeftbtn} align-middle me-2`}
-                    >
-                      <i
-                        className={`${styles.icon_arrowLeftbtn} h-100 p-1 ri-arrow-left-line  border mt-1 rounded `}
-                      ></i>
-                    </span>
+                <div className={`content-btn d-flex justify-content-end mt-5`}>
+                  <div className={`d-flex`}>
+                    <button className={`bg-white border-0  `}>
+                      <span
+                        className={`${styles.container_icon_arrowLeftbtn} align-middle me-2`}
+                      >
+                        <i
+                          className={`${styles.icon_arrowLeftbtn} h-100 p-1 ri-arrow-left-line  border mt-1 rounded `}
+                        ></i>
+                        {/* <Image
+                      src={backButton}
+                      className={`${styles.back_button_image} `}/> */}
+                      </span>
 
-                    <span
-                      className={` ${styles.color_1D72DB} ${styles.fontFam_poppins} ${styles.font_medium} ${styles.font_18} align-middle`}
-                    >
-                      Back
-                    </span>
-                  </button>
+                      <span
+                        className={` ${styles.color_1D72DB} ${styles.fontFam_poppins} ${styles.font_medium} ${styles.font_18} align-middle text-primary`}
+                      >
+                        Back
+                      </span>
+                    </button>
 
-                  <button
-                    type="submit"
-                    className={`${styles.bg_color_1D72DB} bg-primary ms-3 text-white d-flex justify-content-between align-items-center rounded-3 border-0  px-3 py-2`}
-                  >
-                    <span
-                      className={` ${styles.fontFam_poppins} ${styles.font_medium} ${styles.font_18} align-middle`}
+                    <button
+                      type="submit"
+                      className={`${styles.bg_color_1D72DB} bg-primary ms-3 text-white d-flex justify-content-between align-items-center rounded-3 border-0  px-3 py-2`}
                     >
-                      Next
-                    </span>
-                    <div>
-                      <i
-                        className={`${styles.modal_icon_arrowRightbtn} h-100 p-1 ri-arrow-right-line text-white border-light border mt-1 rounded ms-5 `}
-                      ></i>
-                    </div>
-                  </button>
+                      <span
+                        className={` ${styles.fontFam_poppins} ${styles.font_medium} ${styles.font_18} align-middle`}
+                      >
+                        Next
+                      </span>
+                      <div>
+                        <i
+                          className={`${styles.modal_icon_arrowRightbtn} h-100 p-1 ri-arrow-right-line text-white border-light border mt-1 rounded ms-5 `}
+                        ></i>
+                      </div>
+                    </button>
+                  </div>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
