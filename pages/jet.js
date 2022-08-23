a612628441e57f978aecf5d0ca8e6af958a6e0fe
M Preethi <preethi@firstfloor.agency>
2022-08-23 19:18:11 +0530

    latest

--- pages/jet.js
+++ pages/jet.js
@@ -3,6 +3,8 @@ import Button from "react-bootstrap/Button";
 import ApartmentDeletePhoto from "../components/modals/apartmentDeletePhoto";
 import ApartmentEditProperty from "../components/modals/apartmentEditProperty";
 import ApartmentAreaPrice from "../components/modals/apartmentModals/apartmentAreaPrice";
+import ApartmentAreaPriceRent from "../components/modals/apartmentModals/apartmentAreaPriceRent";
+import ApartmentDescription from "../components/modals/apartmentModals/apartmentDescription";
 import ApartmentEditAmenities from "../components/modals/apartmentModals/apartmentEditAmenities";
 import ApartmentUpdateMap from "../components/modals/apartmentUpdateMap";
 
@@ -18,7 +20,9 @@ const Hello = () => {
      <Button onClick={handleShow}>modal</Button>
      </div>
       
-     <ApartmentAreaPrice handleClose={handleClose} show={show} />
+     <ApartmentDescription handleClose={handleClose} show={show} />
+     {/* <ApartmentAreaPriceRent handleClose={handleClose} show={show} /> */}
+     {/* <ApartmentAreaPrice handleClose={handleClose} show={show} /> */}
      {/* <ApartmentEditAmenities handleClose={handleClose} show={show} /> */}
      {/* <ApartmentEditProperty handleClose={handleClose} show={show} /> */}
      {/* <ApartmentUpdateMap handleClose={handleClose} show={show} /> */}
