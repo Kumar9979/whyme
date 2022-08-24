a473f62591f9a631b3e8d578646957feff9c9201
ImanKhan <imankhan@firstfloor.agency>
2022-08-24 19:47:55 +0530

    added some component to propertydetails

--- components/contact_owner/index.js
+++ components/contact_owner/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import styles from "../../styles/propertydetails/contactowner.module.css"
+import React from "react";
+import styles from "../../styles/propertydetails/contactowner.module.css";
 
 const ContactOwner = () => {
   return (
     <div>
-        <div className={`${styles.contact_owner} d-flex justify-content-between px-3 py-2 align-items-center mt-3`}>
-            <div className={`${styles.contact_owner_text} `}>Want to talk with owner?</div>
-            <button className={`${styles.contact_owner_button} px-5 py-2`}>Contact</button>
+      <div
+        className={`${styles.contact_owner} d-flex flex-column flex-sm-row justify-content-between  px-3 py-3  align-items-center mt-3`}
+      >
+        <div
+          className={`${styles.contact_owner_text}  d-flex justify-content-center`}
+        >
+          Want to talk with owner?
         </div>
+        <button className={`${styles.contact_owner_button} px-5 py-2 mt-3 mt-lg-0`}>
+          Contact
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactOwner
+export default ContactOwner;
