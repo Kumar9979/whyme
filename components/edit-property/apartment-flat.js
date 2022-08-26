e2017a2e75c17fe23b51dcdcee4faccfd8c9b081
M Preethi <preethi@firstfloor.agency>
2022-08-26 10:54:14 +0530

    latest

--- components/edit-property/apartment-flat.js
+++ components/edit-property/apartment-flat.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import styles from "../../styles/edit-property/apartment-flat.module.css"
+import Image from "next/image";
+import backIcon from "../../assets/icons/back-icon.svg";
 
 const ApartmentFlat = () => {
   return (
-    <div>apartment-flat</div>
+    <div>
+        <div className={`${styles.back_icon} d-flex justify-content-center`}>
+          <span>
+            {" "}
+            <Image src={backIcon} />
+            <span className={`${styles.back_text} ms-2 fs_15 fw_500 fontFam_poppins`}>Back</span>{" "}
+          </span>
+        </div>
+        <div className={`${styles.card} container d-flex justify-content-center`}>
+            <div className={`${styles.card_body}`}>
+               jhkjk
+            </div>
+
+        </div>
+    </div>
   )
 }
 
