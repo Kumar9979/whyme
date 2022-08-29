8d887391930926ec3fa9b310a7408f0151b4cec8
M Preethi <preethi@firstfloor.agency>
2022-08-29 18:32:36 +0530

    latest

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import styles from "../../styles/dropdown/dropdown.module.css";
 import { motion } from "framer-motion";
-const Dropdown = ({ children, placeholder }) => {
+import { Space } from "antd";
+import Close from "../../assets/icons/close.png";
+import Image from "next/image";
+const Dropdown = ({ children, placeholder, onRemoveTag }) => {
   const [isActive, setIsActive] = useState(false);
   const [selected, setIsSelected] = useState(placeholder);
   const [isHover, toggleHover] = React.useState(false);
+console.log(selected);
   const toggleHoverMenu = () => {
     toggleHover(!isHover);
   };
@@ -12,6 +16,9 @@ const Dropdown = ({ children, placeholder }) => {
   const toggleMouseMenu = () => {
     toggleMouse(!isMouse);
   };
+
+
+
   const subMenuAnimate = {
     enter: {
       opacity: 1,
@@ -34,17 +41,41 @@ const Dropdown = ({ children, placeholder }) => {
     },
   };
   return (
-    <motion.div
-      onHoverStart={toggleHoverMenu}
-      className={`${styles.dropdown}`}
-    >
+    <motion.div onHoverStart={toggleHoverMenu} className={`${styles.dropdown}`}>
       <div
         onClick={(e) => {
           setIsActive(!isActive);
         }}
         className={`${styles.dropdown_btn}`}
       >
-        {selected}
+        {placeholder.length !== 0 ? (
+          placeholder.slice(0, 2).map((item, index) => {
+            return (
+              <div key={index} className={`card`}>
+                <div className="d-flex">
+                  <div className="card-body px-1 py-1">
+                    <div className={``}>{item}</div>
+                  </div>
+                  <button className={`${styles.close_btn} d-flex align-items-center`}>
+                    <Image
+                      src={Close}
+                      onClick={()=>onRemoveTag(index)}
+                      className={``}
+                      width={15}
+                      height={15}
+                    />
+                  </button>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <Space
+            className={`${styles.property_types_text} fs_12 fw_400 ms-2 fontFam_poppins`}
+          >
+            Property Type
+          </Space>
+        )}
         <span className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"} />
       </div>
       <motion.div
--- components/filter-top-navbar/filterTopNavbar.js
+++ components/filter-top-navbar/filterTopNavbar.js
@@ -1,223 +1,233 @@
-// import React, { useState } from "react";
-// import { Dropdown } from "react-bootstrap";
-// import styles from "../../styles/filter-top-navbar/filterTopNavbar.module.css";
-// import DropDown from "../../assets/icons/dropdown.svg";
-// import Image from "next/image";
-// import { Space } from "antd";
-// import Home from "../../assets/icons/home-new.svg";
-
-// const FilterTopNavbar = () => {
-//   const [label, setLabel] = useState("");
-//   const [data, setData] = useState([]);
-
-//   const handleClick = (text) => {
-//     return setData([...data, text]);
-//   };
-
-//   console.log(data);
-
-//   return (
-//     <div className={`${styles.filter_navbar} px-5`}>
-//       <div className={`d-flex`}>
-//         <div>
-//           <div className="dropdown fontFam_poppins">
-//             <button
-//               className={`${styles.buy_btn} btn  my-2`}
-//               type="button"
-//               data-bs-toggle="dropdown"
-//               aria-expanded="false"
-//             >
-//               <Space
-//                 className={`${styles.buy_text} fs_14 fw_500 fontFam_poppins`}
-//               >
-//                 {" "}
-//                 Buy{" "}
-//               </Space>
-
-//               <div
-//                 className={`${styles.dropdown_icon} ms-4 d-flex align-items-center`}
-//               >
-//                 <Image src={DropDown} />
-//               </div>
-//             </button>
-//             <div className="dropdown-menu p-2 fontFam_poppins">
-//               <div className={`d-flex `}>
-//                 <button className={`${styles.second_row_button}`}>
-//                   <span>Sell</span>
-//                 </button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div  className={`${styles.dropDown_property_type}`}>
-//           <div className={`${styles.dropDown_property_type} ms-2 fontFam_poppins`}>
-//             <button
-//               className={`${styles.buy_btn} btn  my-2 `}
-//               type="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false"
-//             >
-//               <div className={`${styles.home_icon} d-flex align-items-center`}>
-//                 <Image src={Home} width={15} height={15} />
-//               </div>
-//               {data.length !== 0 ? (
-//                 data.slice(0,2).map((item, index) => {
-//                   return (
-//                     <div key={index} className={`card`}>
-//                       <div className="card-body">
-//                         <div>{item}</div>
-//                       </div>
-//                     </div>
-//                   );
-//                 })
-//               ) : (
-//                 <Space
-//                   className={`${styles.property_types_text} fs_12 fw_400 ms-2 fontFam_poppins`}
-//                 >
-//                   Property Type
-//                 </Space>
-//               )}
-
-//               <div
-//                 className={`${styles.dropdown_icon} ms-4 d-flex align-items-center`}
-//               >
-//                 <Image src={DropDown} />
-//               </div>
-//             </button>
-//             <div className="dropdown-menu p-2 fontFam_poppins">
-//               <span>Residential</span>
-//               <div className={`d-flex `}>
-//                 <button
-//                   onClick={(e) => handleClick(e.target.textContent)}
-//                   className={`${styles.first_row_button}`}
-//                 >
-//                   <span>Flat</span>
-//                 </button>
-//                 <button
-//                   onClick={(event) => handleClick(event.target.textContent)}
-//                   className={`${styles.first_row_button} ms-2`}
-//                 >
-//                   <span>House/Villa</span>
-//                 </button>
-//                 <button onClick={(e) => handleClick(e.target.textContent)}   className={`${styles.first_row_button} ms-2`}>
-//                   <span>Plot/Land</span>
-//                 </button>
-//               </div>
-
-//               <div className={`mt-2`}>Commercial</div>
-//               <div className={`d-flex `}>
-//                 <button onClick={(e) => handleClick(e.target.textContent)} className={`${styles.second_row_button}`}>
-//                   <span className={`text-nowrap`}>Office Space</span>
-//                 </button>
-//                 <button className={`${styles.second_row_button} ms-2`}>
-//                   <span className={`text-nowrap`}>Shop/Showroom</span>
-//                 </button>
-//               </div>
-//               <div className={`d-flex mt-2`}>
-//                 <button className={`${styles.second_row_button}`}>
-//                   <span className={`text-nowrap`}>Warehouse/Godown</span>
-//                 </button>
-//                 <button className={`${styles.second_row_button} ms-2`}>
-//                   <span className={`text-nowrap`}>Industrial Shed</span>
-//                 </button>
-//               </div>
-//               <div className={`d-flex mt-2`}>
-//                 <button className={`${styles.second_row_button}`}>
-//                   <span className={`text-nowrap`}>Industrial Building</span>
-//                 </button>
-//                 <button className={`${styles.second_row_button} ms-2`}>
-//                   <span className={`text-nowrap`}>Commercial Land</span>
-//                 </button>
-//               </div>
-//               <div className={`mt-2`}>Agriculture</div>
-//               <div className={`d-flex `}>
-//                 <button className={`${styles.second_row_button}`}>
-//                   <span>Agriculture Land</span>
-//                 </button>
-//                 <button className={`${styles.second_row_button} ms-2`}>
-//                   <span>Farm House</span>
-//                 </button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <Dropdown className="d-inline mx-2" autoClose={false}>
-//         <Dropdown.Toggle  >
-//           Manual Close
-//         </Dropdown.Toggle>
-
-//         <Dropdown.Menu>
-//           <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-//           <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-//           <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-//         </Dropdown.Menu>
-//       </Dropdown>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FilterTopNavbar;
-
-
-
-
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "../../styles/filter-top-navbar/filterTopNavbar.module.css";
+import DropDownImage from "../../assets/icons/dropdown.svg";
+import Image from "next/image";
+import { Space } from "antd";
+import Home from "../../assets/icons/home-new.svg";
+import Dropdown from "../dropdown/dropdown";
+import { Select } from "@material-ui/core";
 
 const FilterTopNavbar = () => {
-  const [isActive, setIsActive] = useState(false);
-  const [selected, setIsSelected] = useState("Choose one");
-  function handleBlur(e) {
-    console.log(e);
+  const [label, setLabel] = useState("");
+  const [data, setData] = useState([]);
+  
+
+  const handleClick = (text) => {
+    return setData([...data, text]);
+  };
+
+  const onTagRemove= (index) => {
+  setData([
+      ...data.slice(0, index),
+      ...data.slice( index+ 1, data.length),
+    ]);
   }
+  console.log(data);
+
   return (
     <div className={`${styles.filter_navbar} px-5`}>
       <div className={`d-flex`}>
-        <div
-          onClick={(e) => {
-            setIsActive(!isActive);
-          }}
-          className={`${styles.dropdown_btn} dropdown-btn`}
-        >
-          {selected}
-          <span
-            className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"}
-          />
+        <div>
+          <div className="dropdown fontFam_poppins">
+            <button
+              className={`${styles.buy_btn} btn  my-2`}
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <Space
+                className={`${styles.buy_text} fs_14 fw_500 fontFam_poppins`}
+              >
+                {" "}
+                Buy{" "}
+              </Space>
+
+              <div
+                className={`${styles.dropdown_icon} ms-4 d-flex align-items-center`}
+              >
+                <Image src={DropDownImage} />
+              </div>
+            </button>
+            <div className="dropdown-menu p-2 fontFam_poppins">
+              <div className={`d-flex `}>
+                <button className={`${styles.second_row_button}`}>
+                  <span>Sell</span>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div
-          className={`${styles.dropdown_content} dropdown-content`}
-          style={{ display: isActive ? "block" : "none" }}
-        >
-          
-          <div
-            onClick={(e) => {
-              setIsSelected(e.target.textContent);
-            }}
-            className={`${styles.item} item`}
-          >
-            One
+     
+        <Dropdown placeholder= {data} onRemoveTag={onTagRemove}
+ >
+          <div className="fontFam_poppins">
+            <span>Residential</span>
+            <div className={`d-flex `}>
+              <button
+                onClick={(e) => handleClick(e.target.textContent)}
+                className={`${styles.first_row_button}`}
+              >
+                <span>Flat</span>
+                
+              </button>
+              <button
+                onClick={(event) => handleClick(event.target.textContent)}
+                
+                className={`${styles.first_row_button} ms-2`}
+              >
+                <span>House/Villa</span>
+              </button>
+              <button
+                onClick={(e) => handleClick(e.target.textContent)}
+                className={`${styles.first_row_button} ms-2`}
+              >
+                <span>Plot/Land</span>
+              </button>
+            </div>
+
+            <div className={`mt-2`}>Commercial</div>
+            <div className={`d-flex `}>
+              <button
+                onClick={(e) => handleClick(e.target.textContent)}
+                className={`${styles.second_row_button}`}
+              >
+                <span className={`text-nowrap`}>Office Space</span>
+              </button>
+              <button className={`${styles.second_row_button} ms-2`}>
+                <span className={`text-nowrap`}>Shop/Showroom</span>
+              </button>
+            </div>
+            <div className={`d-flex mt-2`}>
+              <button className={`${styles.second_row_button}`}>
+                <span className={`text-nowrap`}>Warehouse/Godown</span>
+              </button>
+              <button className={`${styles.second_row_button} ms-2`}>
+                <span className={`text-nowrap`}>Industrial Shed</span>
+              </button>
+            </div>
+            <div className={`d-flex mt-2`}>
+              <button className={`${styles.second_row_button}`}>
+                <span className={`text-nowrap`}>Industrial Building</span>
+              </button>
+              <button className={`${styles.second_row_button} ms-2`}>
+                <span className={`text-nowrap`}>Commercial Land</span>
+              </button>
+            </div>
+            <div className={`mt-2`}>Agriculture</div>
+            <div className={`d-flex `}>
+              <button className={`${styles.second_row_button}`}>
+                <span>Agriculture Land</span>
+              </button>
+              <button className={`${styles.second_row_button} ms-2`}>
+                <span>Farm House</span>
+              </button>
+            </div>
           </div>
+        </Dropdown>
+        <div className={`${styles.dropDown_property_type}`}>
           <div
-            className={`${styles.item} item`}
-            onClick={(e) => {
-              setIsSelected(e.target.textContent);
-            }}
+            className={`${styles.dropDown_property_type} ms-2 fontFam_poppins`}
           >
-            Two
-          </div>
-          <div
-           className={`${styles.item} item`}
-            onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-          >
-            Three
+            <button
+              className={`${styles.buy_btn} btn  my-2 `}
+              type="button"
+              data-bs-toggle="dropdown"
+              data-bs-auto-close="outside"
+              aria-expanded="false"
+            >
+              <div className={`${styles.home_icon} d-flex align-items-center`}>
+                <Image src={Home} width={15} height={15} />
+              </div>
+              {data.length !== 0 ? (
+                data.slice(0, 2).map((item, index) => {
+                  return (
+                    <div key={index} className={`card`}>
+                      <div className="card-body">
+                        <div>{item}</div>
+                      </div>
+                    </div>
+                  );
+                })
+              ) : (
+                <Space
+                  className={`${styles.property_types_text} fs_12 fw_400 ms-2 fontFam_poppins`}
+                >
+                  Property Type
+                </Space>
+              )}
+
+              <div
+                className={`${styles.dropdown_icon} ms-4 d-flex align-items-center`}
+              >
+                <Image src={DropDownImage} />
+              </div>
+            </button>
+            <div className="dropdown-menu p-2 fontFam_poppins">
+              <span>Residential</span>
+              <div className={`d-flex `}>
+                <button
+                  onClick={(e) => handleClick(e.target.textContent)}
+                  className={`${styles.first_row_button}`}
+                >
+                  <span>Flat</span>
+                </button>
+                <button
+                  onClick={(event) => handleClick(event.target.textContent)}
+                  className={`${styles.first_row_button} ms-2`}
+                >
+                  <span>House/Villa</span>
+                </button>
+                <button
+                  onClick={(e) => handleClick(e.target.textContent)}
+                  className={`${styles.first_row_button} ms-2`}
+                >
+                  <span>Plot/Land</span>
+                </button>
+              </div>
+
+              <div className={`mt-2`}>Commercial</div>
+              <div className={`d-flex `}>
+                <button
+                  onClick={(e) => handleClick(e.target.textContent)}
+                  className={`${styles.second_row_button}`}
+                >
+                  <span className={`text-nowrap`}>Office Space</span>
+                </button>
+                <button className={`${styles.second_row_button} ms-2`}>
+                  <span className={`text-nowrap`}>Shop/Showroom</span>
+                </button>
+              </div>
+              <div className={`d-flex mt-2`}>
+                <button className={`${styles.second_row_button}`}>
+                  <span className={`text-nowrap`}>Warehouse/Godown</span>
+                </button>
+                <button className={`${styles.second_row_button} ms-2`}>
+                  <span className={`text-nowrap`}>Industrial Shed</span>
+                </button>
+              </div>
+              <div className={`d-flex mt-2`}>
+                <button className={`${styles.second_row_button}`}>
+                  <span className={`text-nowrap`}>Industrial Building</span>
+                </button>
+                <button className={`${styles.second_row_button} ms-2`}>
+                  <span className={`text-nowrap`}>Commercial Land</span>
+                </button>
+              </div>
+              <div className={`mt-2`}>Agriculture</div>
+              <div className={`d-flex `}>
+                <button className={`${styles.second_row_button}`}>
+                  <span>Agriculture Land</span>
+                </button>
+                <button className={`${styles.second_row_button} ms-2`}>
+                  <span>Farm House</span>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default FilterTopNavbar;
