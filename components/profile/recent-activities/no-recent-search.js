8b8337fb5f5479b1c1386320109170e56fdc34aa
ImanKhan <imankhan@firstfloor.agency>
2022-09-03 19:04:55 +0530

    added some component to propertydetails

---
+++ components/profile/recent-activities/no-recent-search.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../../../styles/profile/recent-activities/recent-search.module.css";
+import view from "../../../assets/icons/profile-icons/view.svg";
+import Image from "next/image";
+
+
+const NoRecentSearch = () => {
+  return (
+    <div className="d-flex h-100 flex-column d-flex justify-content-center align-items-center pe-5">
+      <Image src={view} alt="Picture of the author" width={50} />
+      <h2
+        className={`${styles.recent_search_text} fs_24 fontFam_poppins fw_500`}
+      >
+        No Recent Searches
+      </h2>
+      <p className={`${styles.explore_search} fs_16 fontFam_poppins fw_400`}>
+        We guess this is your first time! explore properties now.
+      </p>
+      <button
+        className={`${styles.explore_button} fs_13 fontFam_poppins fw_400 px-5 py-1`}
+      >
+        Explore
+      </button>
+    </div>
+  );
+};
+
+export default NoRecentSearch;
---
+++ components/profile/recent-activities/recent-search.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../../styles/profile/recent-activities/recent-search.module.css";
+import view from "../../../assets/icons/profile-icons/view.svg";
+import Image from "next/image";
+
+
+const RecentSearch = () => {
+  return (
+    <div className="d-flex h-100 flex-column ">
+      fdsfjkbfsjkbgk
+    </div>
+  );
+};
+
+export default RecentSearch;
