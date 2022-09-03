e039df52c0c613a1e5c3e16a075b20191926b506
ImanKhan <imankhan@firstfloor.agency>
2022-09-03 09:00:48 +0530

    added some component to propertydetails

---
+++ pages/profile-pages/agent-my-profile.js
@@ -0,0 +1,248 @@
+import { React } from "react";
+import ProfileLayout from "../../components/sidebarLayout/Sidebar";
+import styles from "../../styles/profile-pages/agent-my-profile.module.css";
+import arrow_left from "../../assets/images/arrow_left.svg";
+import Image from "next/image";
+import people from "../../assets/images/imagereview/people.png";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import verified from "../../assets/icons/edit-profile-icons/verified.svg";
+import { useRouter } from "next/router";
+
+const MyProfile = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "Iman Khan",
+      phone: "6360749419",
+      email: "imankhan.coorg@gmail.com",
+      city: "Mysore",
+      message: "lorem ipsum",
+    },
+    // validationSchema: Yup.object({
+    //   name: Yup.string()
+    //     .matches(nameregex, "Invalid First Name")
+    //     .required("Required"),
+    //   phone: Yup.string()
+    //     .matches(phoneregex, "Invalid Phone Number")
+    //     .required("Required"),
+    //   email: Yup.string().email().required("Required"),
+    // }),
+    // onSubmit: (values, { resetForm }) => {
+    //   console.log(values);
+    //   localStorage.setItem("User", JSON.stringify(values));
+    //   alert(values);
+    //   resetForm();
+    // },
+  });
+  const router = useRouter();
+
+  return (
+    <ProfileLayout>
+      <div className={`${styles.edit_profile} me-lg-5 me-0`}>
+        <div className={`${styles.edit_profile_card} p-lg-4 mt-3 p-3 `}>
+          <div className="d-flex align-items-center ">
+            <button
+              className={`${styles.arrow_left_button} d-flex align-items-center p-0`}
+            >
+              <Image
+                src={arrow_left}
+                alt="Picture of the author"
+                width={20}
+                height={15}
+              />
+            </button>
+            <span
+              className={`${styles.edit_profile_heading} fs_20 fw_400 ms-2`}
+            >
+              My Profile
+            </span>
+          </div>
+          <hr className={`${styles.edit_horizontal_line}`}></hr>
+
+          <div className="row ms-lg-4 ms-0 ">
+            <div className="col-12">
+              <div className="row ">
+                <div className="col-6 col-lg-3 d-flex justify-content-center ">
+                  <div className="d-flex flex-column">
+                    <h1
+                      className={`${styles.profile_type} fs_15  d-block d-lg-none fw_500 mt-lg-0 d-flex justify-content-start fontFam_poppins`}
+                    >
+                      Profile Type{" "}
+                      <span className={`${styles.type_buyer} fw_600 ps-3`}>
+                      Agent
+                      </span>
+                    </h1>
+                    <h3
+                      className={`${styles.profile_picture_heading} mt-2 mt-lg-0 fs_15 fw_500 fontFam_poppins`}
+                    >
+                      Profile Picture
+                    </h3>
+                    <div className="">
+                      <Image
+                        src={people}
+                        className={`${styles.profile_photo}`}
+                        alt="Picture of the author"
+                        // width={60}
+                        height={210}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-12 col-lg-9 d-flex justify-content-center justify-content-lg-start">
+                  <div className={`${styles.form_width}`}>
+                    <div className="ps-lg-2 ps-0">
+                      <h1
+                        className={`${styles.profile_type} fs_15 d-none d-lg-block fw_500 mt-4 mt-lg-0 d-flex justify-content-center fontFam_poppins`}
+                      >
+                        Profile Type{" "}
+                        <span className={`${styles.type_buyer} fw_600 ps-3`}>
+                          Buyer
+                        </span>
+                      </h1>
+
+                      <form>
+                        <div className="">
+                          <div className="row">
+                            <div className="col-6">
+                              <div className="form-group mt-3">
+                                <label
+                                  className={`${styles.contact_us_label}`}
+                                  for="exampleInputEmail1"
+                                >
+                                  Full Name
+                                </label>
+
+                                <input
+                                  type="text"
+                                  readOnly
+                                  className={` ${styles.form_input} w-100 py-1 mt-1 ps-3 px-5 fs_15 fw_600 fontFam_poppins`}
+                                  id="exampleInputEmail1"
+                                  aria-describedby="emailHelp"
+                                  placeholder="Enter your name"
+                                  name="name"
+                                  value={formik.values.name}
+                                />
+                              </div>
+                              <div className="form-group mt-3">
+                                <label
+                                  className={`${styles.contact_us_label} `}
+                                  for="exampleInputEmail1"
+                                >
+                                  Email Address
+                                </label>
+                                <div
+                                  className={`${styles.verified_input} w-100 py-1 mt-1 d-flex justify-content-between `}
+                                >
+                                  <input
+                                    type="email"
+                                    readOnly
+                                    className={` ${styles.email_input} w-75 ps-3  fs_15 fw_600 fontFam_poppins`}
+                                    id="exampleInputEmail1"
+                                    aria-describedby="emailHelp"
+                                    placeholder="Enter your email address"
+                                    name="email"
+                                    value={formik.values.email}
+                                  />
+                                  <span className="d-flex align-items-center pe-2">
+                                    <Image
+                                      src={verified}
+                                      alt="Picture of the author"
+                                      width={13}
+                                      height={13}
+                                    />
+                                    <span
+                                      className={`${styles.verified_text} ps-1 fw_500 fs_13 fontFam_poppins`}
+                                    >
+                                      verified
+                                    </span>
+                                  </span>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div className="col-6">
+                              <div className="form-group mt-3">
+                                <label
+                                  className={`${styles.contact_us_label}`}
+                                  for="exampleInputPassword1"
+                                >
+                                  Mobile Number
+                                </label>
+                                <input
+                                  readOnly
+                                  type="text"
+                                  className={` ${styles.form_input} w-100 mt-1 ps-3 py-1 fs_15 fw_600 fontFam_poppins`}
+                                  id="exampleInputPassword1"
+                                  placeholder="Enter your contact number"
+                                  name="phone"
+                                  value={formik.values.phone}
+                                />
+                              </div>
+                              <div className="form-group mt-3">
+                                <label
+                                  className={`${styles.contact_us_label}`}
+                                  for="exampleInputEmail1"
+                                >
+                                  City
+                                </label>
+                                <input
+                                  readOnly
+                                  type="text"
+                                  className={` ${styles.form_input} mt-1 w-100 py-1 ps-3 fs_15 fw_600 fontFam_poppins`}
+                                  id="exampleInputEmail1"
+                                  aria-describedby="emailHelp"
+                                  placeholder="Enter your name"
+                                  name="city"
+                                  value={formik.values.city}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div id="comment-message" className="form-group mt-3">
+                            <label
+                              className={`${styles.contact_us_label} `}
+                              htmlFor="exampleInputEmail1"
+                            >
+                              Message
+                            </label>
+                            <textarea
+                              readOnly
+                              className={`${styles.description_box} w-100 mt-1 ps-2  fs_13 fw_500 fontFam_poppins`}
+                              placeholder="Message"
+                              id="exampleInputEmail1"
+                              name="message"
+                              value={formik.values.message}
+                            ></textarea>
+                          </div>
+                          <div className="d-flex justify-content-between mt-4 w-75 ">
+                            <button
+                              type="button"
+                              onClick={() => {
+                                router.push("/profile-pages/edit-profile");
+                              }}
+                              className={`${styles.save_button_width} px-5 py-1 fs_15 fw_400`}
+                            >
+                              Edit Profile
+                            </button>
+                            {/* <button
+                            className={`${styles.undo_button_width}  px-3 py-1 ms-4 fs_15 fw_500`}
+                          >
+                            Undo Changes
+                          </button> */}
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </ProfileLayout>
+  );
+};
+
+export default MyProfile;
