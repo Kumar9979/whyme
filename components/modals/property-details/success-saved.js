66444a3267dff1e7ab44c42b4e40bdf7a2eee68e
First floor <karthikkumar@firstfloor.agency>
2022-09-08 09:55:34 +0530

    added components

--- components/modals/property-details/success-saved.js
+++ components/modals/property-details/success-saved.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
-// import styles from "../../../styles/modals/edit-profile-modals/edit-profile.module.css";
-import Styles from "../../../styles/modals/property-details-modals/reportProperty.module.css";
-import styles2 from "../../../styles/modals/property-details-modals/shareproperty.module.css";
-// import { useFormik } from "formik";
-// import * as Yup from "yup";
+import successsv from "../../../styles/modals/property-details-modals/successsaved.module.css"
 import Image from "next/image";
 import success from "../../../assets/icons/modals/success.svg";
 
@@ -16,11 +12,11 @@ const SuccessSave = ({ show, handleClose }) => {
       centered
       show={show}
       onHide={handleClose}
-      dialogClassName={`${styles2.shareproperty_container}`}
+      dialogClassName={`${successsv.shareproperty_container}`}
       
     >
-      <Modal.Body className={`${styles2.shareproperty_container}`}>
-        <div className= {`${styles2.shareproperty_containerr} my-4 justify-content-center`}  >
+      <Modal.Body className={`${successsv.success_container}`}>
+        <div className= {`${successsv.success_containerr} my-4 justify-content-center`}  >
                 <div className="d-flex justify-content-center"> <Image
                     src={success}
                     alt="Picture of the author"
@@ -30,7 +26,7 @@ const SuccessSave = ({ show, handleClose }) => {
                   />
                   </div>
                   <div className="d-flex justify-content-center">
-                  <span className={`${Styles.report_texthead} mt-lg-3 fs_20 fw_600`}>Changes Saved Successfully</span>
+                  <span className={`${successsv.success_texthead} mt-lg-3 mt-md-3 mt-sm-3 mt-3 fs_20 fw_600`}>Changes Saved Successfully</span>
                   </div>
         </div>
       </Modal.Body>
