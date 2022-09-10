8f92b66d6bced90a85d5e5efa9037ac62c799223
ImanKhan <imankhan@firstfloor.agency>
2022-09-10 16:12:11 +0530

    added some component to propertydetails

--- components/property-details/amenties/index.js
+++ components/property-details/amenties/index.js
@@ -1,90 +1,96 @@
-import React from 'react';
-import styles from "../../../styles/propertydetails/amenties.module.css"
+import React from "react";
+import { useState } from "react";
+import styles from "../../../styles/propertydetails/amenties.module.css";
 
-const Amenties = () => {
+const Amenties = ({ data }) => {
+  const [properties, setProperties] = useState(
+    data == 1 ? plotAmenties : amenties
+  );
   return (
     <div className={`${styles.amenties} p-4 mt-4`}>
       <p className={`${styles.amenties_text}`}>Amenities</p>
       <hr className={`${styles.amenties_horizontal}`}></hr>
-      <div className='row'>
+      <div >
         {
-          amenties.map((item, index) => {
-            return (
-              <div className='col-lg-4 col-12 '>
-                <ul className={`${styles.amenties_bullet} d-flex align-items-center`}>
-                  <li className={`${styles.amenties_list}  ps-2 `}>{item.list}</li>
-                </ul>
-              </div>
-            );
-          })
+          <div className="row">
+            {properties.map((item, index) => {
+              return (
+                <div className="col-lg-4 col-12 ">
+                  <ul
+                    className={`${styles.amenties_bullet} d-flex align-items-center`}
+                  >
+                    <li className={`${styles.amenties_list}  ps-2 `}>
+                      {item.list}
+                    </li>
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
         }
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Amenties;
 
 const amenties = [
   {
-   list: "Elevators/Lifts"    
+    list: "Elevators/Lifts",
   },
-  
-  {
-   list: "Swimming Pool"    
-  },
-  
-  {
-   list: "Yoga Meditation Area"    
-  },
-  
-  {
-   list: " Guest Parking Spaces"    
-  },
-  
-  {
-   list: "Gym"    
-  },
-  
-  {
-   list: "Pet Room"    
-  },
-  
-  {
-   list: "CCTV Surveillance"    
-  },
-  
-  {
-   list: "Play Ground"    
-  }
-  
-]
 
+  {
+    list: "Swimming Pool",
+  },
+
+  {
+    list: "Yoga Meditation Area",
+  },
+
+  {
+    list: " Guest Parking Spaces",
+  },
+
+  {
+    list: "Gym",
+  },
+
+  {
+    list: "Pet Room",
+  },
+
+  {
+    list: "CCTV Surveillance",
+  },
+
+  {
+    list: "Play Ground",
+  },
+];
 
 const plotAmenties = [
   {
-   list: "Basic Amenities"    
+    list: "Basic Amenities",
   },
-  
+
   {
-   list: "Underground Drainage System"    
+    list: "Underground Drainage System",
   },
-  
+
   {
-   list: "Highway Access"    
+    list: "Highway Access",
   },
-  
+
   {
-   list: "Main Road Access"    
+    list: "Main Road Access",
   },
-  
+
   {
-   list: "Electricity Line"    
+    list: "Electricity Line",
   },
-  
+
   {
-   list: "Pet Room"    
-  },  
-]
+    list: "Pet Room",
+  },
+];
