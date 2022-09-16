124e71afd1aba8c19d1e79954c3a7cc13cedfc01
abijith <abijith@firstfloor.agency>
2022-09-16 10:15:06 +0530

    laetst

--- pages/post-property/propertyUpload/propertyUpload.js
+++ pages/post-property/propertyUpload/propertyUpload.js
@@ -5,10 +5,13 @@ import * as Yup from "yup";
 import { useFormik } from "formik";
 import upload from "../../../assets/icons/upload.png";
 import backButton from "../../../assets/icons/backbutton.png";
+import PostPropertySubmitButton from "../../../components/postproperty/components/submitButton";
 import { useRouter } from "next/router";
 import StepperNew from "../../stepper/stepper";
 import ImageUploading from "react-images-uploading";
 import closeIcon from "../../../assets/icons/close.png";
+import { FormikErrorGenerator } from "../../../components/postproperty/formData/formikErrorGenerator";
+import PostPropertyLayout from "../../../components/postproperty/components/propertyLayout";
 
 const PropertyUpload = () => {
   const [images, setImages] = useState([]);
@@ -53,255 +56,187 @@ const PropertyUpload = () => {
   };
 
   return (
-    <div>
-      <div className={`${styles.whole_container} container mb-5 mb-lg-0`}>
-        <div className="row justify-content-center mx-2 mx-lg-0">
-          <div
-            className={`col-lg-3 col-md-11 col-sm-11 ${styles.property_boxShadow} ${styles.stepper_border_radius} ${styles.bg_color_1D72DB}`}
-          >
-            <StepperNew currentPage={currentPage} />
-          </div>
-          <div
-            className={` ${styles.property_boxShadow} ${styles.propertyFeature_border_radius}  col-lg-7 col-md-11 col-sm-11`}
-          >
-            {/* <div className={`col-lg-6 col-md-10 col-sm-10 `}> */}
-            <div className={`${styles.input_container}`}>
-              <h5 className={`${styles.propertyHeading} mt-md-5 mt-sm-3 mt-3 `}>
-                Photos & Descrption
-              </h5>
-              {/* <FileUploader handleChange={handleChange} multiple={true} name="file" types={fileTypes} /> */}
-              <form onSubmit={formik.handleSubmit} className="">
-                <div className="mt-5">
-                  <label
-                    htmlFor="profile"
-                    className={`${styles.labelText} form-label`}
-                  >
-                    Photos
-                  </label>
-                  {/* <FileUploader onUpload={onUpload} count={5}> */}
-                  <ImageUploading
-                    multiple
-                    value={images}
-                    onChange={onChange}
-                    maxNumber={maxNumber}
-                    dataURLKey="data_url"
-                  >
-                    {({
-                      imageList,
-                      onImageUpload,
-                      onImageRemoveAll,
-                      onImageUpdate,
-                      onImageRemove,
-                      isDragging,
-                      dragProps,
-                      errors,
-                    }) => (
-                      <div {...dragProps} className={`${styles.upload_image} `}>
-                        {images.length === 0 ? (
-                          <div>
-                            <div
-                              htmlFor="upload"
-                              className="d-flex justify-content-center mt-3 mb-2"
-                            >
-                              <label htmlFor="upload">
-                                {" "}
-                                <Image
-                                  src={upload}
-                                  name="image"
-                                  value={formik.values.image}
-                                  alt="image of camera"
-                                />
-                              </label>
-                            </div>
-                            <div className="d-flex flex-column align-items-center justify-content-center ">
-                              <p className={`${styles.upload_text_1}`}>
-                                Drag & drop the image of an property
-                              </p>
-
-                              <p className={`${styles.upload_text_2}`}>
-                                JPG and PNG images - max 20MB each
-                              </p>
-                              <p className={`${styles.upload_text_2}`}>
-                                <span className={`${styles.or_text}`}>-</span>OR
-                                <span>-</span>
-                              </p>
-                              <button
-                                {...dragProps}
-                                onClick={onImageUpload}
-                                className={`${styles.upload_button}`}
-                                type="button"
-                                variant="contained"
-                                component="label"
-                              >
-                                Browse Photos
-                              </button>
-                              {/* <p className={`${styles.upload_text_3}`}>Browse Photos</p> */}
-                            </div>
-                          </div>
-                        ) : (
-                          <>
-                            {" "}
-                            <div className={`row p-2 ps-4`}>
-                              {/* {imageList.slice(0,6).map((image, index) => ( */}
-                              {images.map((image, index) => (
-                                <div
-                                  className={`col-4 d-flex mb-2`}
-                                  key={index}
-                                >
-                                  <div
-                                    className={`${styles.property_upload_preview_image_container}`}
-                                  >
-                                    <Image
-                                      src={image.data_url}
-                                      name="uploaded-images"
-                                      layout="fill"
-                                      objectFit="cover"
-                                      className={`${styles.property_upload_preview_image}`}
-                                    />
-                                  </div>
-                                  <button
-                                    type="button"
-                                    className={`border-0 bg-white`}
-                                    onClick={() => onImageRemove(index)}
-                                  >
-                                    <Image
-                                      src={closeIcon}
-                                      alt="remove image icon"
-                                      width={18}
-                                      height={18}
-                                    />
-                                  </button>
-                                </div>
-                              ))}
-                            </div>
-                            {images.length > 5 ? (
-                              <span>{imgTot - 6} more images</span>
-                            ) : null}
-                            <div
-                              className={`d-flex justify-content-center  mt-4`}
-                            >
-                              <button
-                                {...dragProps}
-                                onClick={onImageUpload}
-                                className={`${styles.upload_button}`}
-                                type="button"
-                                variant="contained"
-                                component="label"
-                              >
-                                Browse Photos
-                              </button>
-                            </div>
-                          </>
-                        )}
-                      </div>
-                    )}
-                  </ImageUploading>
-                </div>
-                {formik.errors.image && formik.touched.image && (
-                  <div className="d-flex align-items-center text-danger mt-1">
-                    <i
-                      className={` ${styles.error_message} ri-error-warning-line me-1 `}
-                    ></i>
-                    <span className={`${styles.error_message}`}>
-                      {formik.errors.image}
-                    </span>
-                  </div>
-                )}
-
-                <div className="mt-5">
-                  <div className="mt-3">
-                    <label
-                      htmlFor="exampleFormControlInput1"
-                      className={`${styles.labelText} form-label`}
-                    >
-                      Description
-                    </label>
-                    <textarea
-                      type="text"
-                      className={`${styles.description} form-control`}
-                      id="exampleFormControlInput1"
-                      aria-describedby="emailHelp"
-                      placeholder="Describe your property"
-                      name="description"
-                      value={formik.values.description}
-                      onChange={formik.handleChange}
-                    ></textarea>
-                  </div>
-                </div>
-                {formik.errors.description && formik.touched.description && (
-                  <div className="d-flex align-items-center text-danger mt-1">
-                    <i className="ri-error-warning-line me-1 0 "></i>
-                    <span>{formik.errors.description}</span>
-                  </div>
-                )}
-                <div className={` mt-2 `}>
-                  <input
-                    type="checkbox"
-                    name="terms"
-                    onChange={formik.handleChange}
-                  />
-                  <span className={`ms-2 ${styles.terms_condition_text}`}>
-                    Terms and condition
-                  </span>
-                </div>
-                {formik.errors.terms && formik.touched.terms && (
-                  <div className="d-flex align-items-center text-danger mt-1">
-                    <i className="ri-error-warning-line me-1 0 "></i>
-                    <span>{formik.errors.terms}</span>
-                  </div>
-                )}
-
-                <div
-                  className={`content-btn d-flex justify-content-end mt-5 mb-3`}
-                >
-                  <div className={`d-flex`}>
-                    <button
-                      type="button"
-                      onClick={() =>
-                        router.push(
-                          "/post-property/price-details/priceDetailsSell"
-                        )
-                      }
-                      className={`bg-white border-0  `}
-                    >
-                      <span
-                        className={`${styles.container_icon_arrowLeftbtn} align-middle me-2`}
-                      >
-                        <i
-                          className={`${styles.icon_arrowLeftbtn} h-100 p-1 ri-arrow-left-line  border mt-1 rounded `}
-                        ></i>
-                      </span>
-
-                      <span
-                        className={` color_light_blue fontFam_poppins ${styles.font_medium} ${styles.font_18} align-middle text-primary`}
-                      >
-                        Back
-                      </span>
-                    </button>
-
-                    <button
-                      type="submit"
-                      className={`${styles.bg_color_1D72DB}  ms-3 text-white d-flex justify-content-between align-items-center rounded-3 border-0  px-3 py-2`}
-                    >
-                      <span
-                        className={` fontFam_poppins ${styles.font_medium} ${styles.font_18} align-middle`}
-                      >
-                        Next
-                      </span>
+    <PostPropertyLayout>
+      <div
+        className={` ${styles.property_boxShadow} ${styles.propertyFeature_border_radius} px-3 px-xl-5 pb-4 col-12 col-xl-6 col-lg-7  `}
+      >
+        {/* <div className={`col-lg-6 col-md-10 col-sm-10 `}> */}
+        <div className={`${styles.input_container} mt-4 mt-sm-0`}>
+          <h5 className={`${styles.propertyHeading} mt-md-5 mt-sm-3 mt-1 `}>
+            Photos & Descrption
+          </h5>
+          {/* <FileUploader handleChange={handleChange} multiple={true} name="file" types={fileTypes} /> */}
+          <form onSubmit={formik.handleSubmit} className="">
+            <div className="mt-4 ">
+              <label
+                htmlFor="profile"
+                className={`${styles.labelText} form-label`}
+              >
+                Photos
+              </label>
+              {/* <FileUploader onUpload={onUpload} count={5}> */}
+              <ImageUploading
+                multiple
+                value={images}
+                onChange={onChange}
+                maxNumber={maxNumber}
+                dataURLKey="data_url"
+              >
+                {({
+                  imageList,
+                  onImageUpload,
+                  onImageRemoveAll,
+                  onImageUpdate,
+                  onImageRemove,
+                  isDragging,
+                  dragProps,
+                  errors,
+                }) => (
+                  <div {...dragProps} className={`${styles.upload_image} `}>
+                    {images.length === 0 ? (
                       <div>
-                        <i
-                          className={`${styles.modal_icon_arrowRightbtn} h-100 p-1 ri-arrow-right-line text-white border-light border mt-1 rounded ms-5 `}
-                        ></i>
+                        <div
+                          htmlFor="upload"
+                          className="d-flex justify-content-center mt-3 mb-2"
+                        >
+                          <label htmlFor="upload">
+                            {" "}
+                            <Image
+                              src={upload}
+                              name="image"
+                              value={formik.values.image}
+                              alt="image of camera"
+                              width={50}
+                              height={40}
+                            />
+                          </label>
+                        </div>
+                        <div className="d-flex flex-column align-items-center justify-content-center ">
+                          <p className={`${styles.upload_text_1}`}>
+                            Drag & drop the image of an property
+                          </p>
+
+                          <p className={`${styles.upload_text_2}`}>
+                            JPG and PNG images - max 20MB each
+                          </p>
+                          <p className={`${styles.upload_text_2}`}>
+                            <span className={`${styles.or_text}`}>-</span>OR
+                            <span>-</span>
+                          </p>
+                          <button
+                            {...dragProps}
+                            onClick={onImageUpload}
+                            className={`${styles.upload_button}`}
+                            type="button"
+                            variant="contained"
+                            component="label"
+                          >
+                            Browse Photos
+                          </button>
+                          {/* <p className={`${styles.upload_text_3}`}>Browse Photos</p> */}
+                        </div>
                       </div>
-                    </button>
+                    ) : (
+                      <>
+                        {" "}
+                        <div   {...dragProps}
+                            onClick={onImageUpload} className={`row p-2 ps-4`}>
+                          {/* {imageList.slice(0,6).map((image, index) => ( */}
+                          {images.map((image, index) => (
+                            <div className={`col-4 d-flex mb-2`} key={index}>
+                              <div
+                                className={`${styles.property_upload_preview_image_container}`}
+                              >
+                                <Image
+                                  src={image.data_url}
+                                  name="uploaded-images"
+                                  layout="fill"
+                                  objectFit="cover"
+                                  className={`${styles.property_upload_preview_image}`}
+                                />
+                              </div>
+                              <button
+                                type="button"
+                                className={`border-0 bg-white`}
+                                onClick={() => onImageRemove(index)}
+                              >
+                                <Image
+                                  src={closeIcon}
+                                  alt="remove image icon"
+                                  width={18}
+                                  height={18}
+                                />
+                              </button>
+                            </div>
+                          ))}
+                        </div>
+                      
+                        {/* <div className={`d-flex justify-content-center  mt-4`}>
+                          <button
+                          
+                            className={`${styles.upload_button}`}
+                            type="button"
+                            variant="contained"
+                            component="label"
+                          >
+                            Browse Photos
+                          </button>
+                        </div> */}
+                      </>
+                    )}
                   </div>
-                </div>
-              </form>
+                )}
+              </ImageUploading>
             </div>
-          </div>
+            <FormikErrorGenerator
+              formikError={formik.errors.image}
+              formikTouched={formik.touched.image}
+            />
+
+
+              <div className="mt-2">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className={`${styles.labelText} form-label`}
+                >
+                  Description
+                </label>
+                <textarea
+                  type="text"
+                  className={`${styles.description} form-control`}
+                  id="exampleFormControlInput1"
+                  aria-describedby="emailHelp"
+                  placeholder="Describe your property"
+                  name="description"
+                  value={formik.values.description}
+                  onChange={formik.handleChange}
+                ></textarea>
+              </div>
+       
+            <FormikErrorGenerator
+              formikError={formik.errors.description}
+              formikTouched={formik.touched.description}
+            />
+            <div className={` mt-1`}>
+              <input
+                type="checkbox"
+                name="terms"
+                onChange={formik.handleChange}
+              />
+              <span className={`ms-2 ${styles.terms_condition_text}`}>
+                Terms and condition
+              </span>
+            </div>
+            <FormikErrorGenerator
+              formikError={formik.errors.terms}
+              formikTouched={formik.touched.terms}
+            />
+
+            <PostPropertySubmitButton paddingTop="1rem" marginTop="mt-2" />
+          </form>
         </div>
       </div>
-    </div>
+    </PostPropertyLayout>
   );
 };
 
