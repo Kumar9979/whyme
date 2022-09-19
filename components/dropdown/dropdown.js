d1f335598ce6cd0a5341d3470dbfb2b1dbb27962
M Preethi <ramugs@firstfloor.agency>
2022-09-19 19:23:38 +0530

    latest

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 import styles from "../../styles/dropdown/dropdown.module.css";
 import { motion } from "framer-motion";
-const Dropdown = ({ children, placeholder, onDelete, data }) => {
+import { Space } from "antd";
+import CloseIcon from "../../assets/icons/close-new-icon.svg";
+import Image from "next/image";
+import HomeIcon from "../../assets/icons/home-green-icon.svg";
+import DropDownImage from "../../assets/icons/dropdown.svg";
+
+const Dropdown = ({ children, placeholder = [], onRemoveTag }) => {
   const [isActive, setIsActive] = useState(false);
   const [selected, setIsSelected] = useState(placeholder);
 
@@ -37,8 +43,8 @@ const Dropdown = ({ children, placeholder, onDelete, data }) => {
       opacity: 0,
       rotateX: -15,
       transition: {
-        duration: 0.1,
-        delay: 0.1,
+        duration: 0.5,
+        delay: 0.3,
       },
       transitionEnd: {
         display: "none",
@@ -46,26 +52,57 @@ const Dropdown = ({ children, placeholder, onDelete, data }) => {
     },
   };
   return (
-    <motion.div onHoverStart={toggleHoverMenu} className={`${styles.dropdown}`}>
-      <div className={`${styles.dropdown_btn}`}>
-        {data.length !== 0
-          ? data.slice(0, 3).map((item, index) => {
-              return (
-                <div key={index} className="card">
-                  <div className="card-body">
-                    <div className="d-flex align-items-center">
-                     <div>{item}</div> 
-                      <i
-                        onClick={() => onDelete(index)}
-                        className="ri-close-line ms-2"
-                      ></i>
-                    </div>
+    <motion.div onHoverStart={()=>toggleHoverMenu("hovered")}
+    onHoverEnd={()=>toggleHoverMenu("leaved")} className={`${styles.dropdown}`}>
+      <div
+        onClick={(e) => {
+          setIsActive(!isActive);
+
+        }}
+        
+        
+        className={`${styles.dropdown_btn} my-2 fw_500 py-2`}
+      >
+        <div className={`${styles.home_icon} d-flex align-items-center ps-2`}>
+          <Image src={HomeIcon} width={15} height={15} />
+        </div>
+        {placeholder?.length !== 0 ? (
+          placeholder?.slice(0, 2).map((item, index) => {
+            return (
+              <div key={index} className={`${styles.card} card ms-2`}>
+                <div className="d-flex ">
+                  <div className={`${styles.card_box} card-body px-1 py-0 d-flex`}>
+                    <div className={`${styles.card_items} fs_9 p-1 fw_500 fontFam_poppins`}>{item}</div>
+                 
+                  <button
+                    className={`${styles.close_btn} d-flex align-items-center`}
+                  >
+                    <Image
+                      src={CloseIcon}
+                      onClick={() => onRemoveTag(index)}
+                      className={``}
+                      width={8}
+                      height={8}
+                    />
+                  </button>
                   </div>
                 </div>
-              );
-            })
-          : "Select"}
-        <i className={isHover?"ri-arrow-up-s-fill":"ri-arrow-down-s-fill"}></i>
+              </div>
+            );
+          })
+        ) : (
+          <Space
+            className={`${styles.property_types_text} fs_12 fw_400 ms-2 fontFam_poppins d-flex justify-content-start `}
+          >
+            Property types
+          </Space>
+        )}
+        <div
+          className={`${styles.dropdown_icon} ms-auto me-2 d-flex align-items-center `}
+        >
+          <Image src={DropDownImage} />
+        </div>
+        <span className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"} />
       </div>
       <motion.div
         animate={isHover ? "enter" : "exit"}
