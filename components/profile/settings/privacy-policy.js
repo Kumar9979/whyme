bf4fd97a5f4cb5d862763e0143ae7be737d3ac40
Ashish Nagar <ashish.nagar@firstfloor.agency>
2022-10-10 19:15:30 +0530

    text color of privacy policy & tnconditon

--- components/profile/settings/privacy-policy.js
+++ components/profile/settings/privacy-policy.js
@@ -7,7 +7,7 @@ const PrivacyPolicy = () => {
       <h1 className={`color_light_blue fw_600 fs_24 fontFam_poppins d-none d-md-block`}>
      Privacy Policy
       </h1>
-      <p  className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p  className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
@@ -19,12 +19,12 @@ const PrivacyPolicy = () => {
         ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
         elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
@@ -60,12 +60,12 @@ const PrivacyPolicy = () => {
         dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
         est Lorem ipsum dolor sit amet.
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum
--- components/profile/settings/terms-condition.js
+++ components/profile/settings/terms-condition.js
@@ -7,7 +7,7 @@ const TermsAndCondition = () => {
       <h1 className={`color_light_blue fw_600 fs_24 fontFam_poppins d-none d-md-block`}>
       Terms & Condition
       </h1>
-      <p  className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p  className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
@@ -19,12 +19,12 @@ const TermsAndCondition = () => {
         ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
         elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
@@ -60,12 +60,12 @@ const TermsAndCondition = () => {
         dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
         est Lorem ipsum dolor sit amet.
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum
       </p>
-      <p className={`color_black fs_14 fontFam_poppins fw_500 mb-3`}>
+      <p className={`color_grey fs_14 fontFam_poppins fw_500 mb-3`}>
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum
